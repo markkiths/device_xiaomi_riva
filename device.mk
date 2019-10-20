be5078110905cbbba70ec00186f05b5286dfd31b
intervigilium <intervigil@gmail.com>
2019-10-20 07:42:26 +0530

    riva: Use Soong namespace to avoid device build conflicts

--- device.mk
+++ device.mk
@@ -346,6 +346,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(DEVICE_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+	
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += device/xiaomi/riva
 
 # Telephony
 PRODUCT_PACKAGES += \
