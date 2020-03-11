904e44e91190fb34d22120020a3e676a39ff5f0e
SunnyRaj84348 <sunnyraj7461@gmail.com>
2020-03-11 07:19:16 +0000

    riva: Update GPS to LA.UM.8.6.r1-04000-89xx.0
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- device.mk
+++ device.mk
@@ -162,9 +162,13 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
+    libbatching \
+    libgeofencing \
     libgnss \
     libgnsspps \
-    libsensorndkbridge
+    libjson \
+    libsensorndkbridge \
+    libwifi-hal-ctrl
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
